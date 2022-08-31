e97fb40adefe5bad2dbb10c05a182d9da700d03f
shatong <843391622@qq.com>
2022-08-31 15:03:50 +0800

    feat：移除业务转换部分

--- src/parse/index.ts
+++ src/parse/index.ts
@@ -1,20 +1,31 @@
-import { MusicInfo, MusicScore, Note } from '../type';
+import { MusicScore, Note } from '../type';
 
 /**
  * 解析abc乐谱
  * @param {string} txt
- * @return {MusicScore}
+ * @return {MusicScore[]}
  */
-export function parse(txt: string): MusicScore {
+export function parse(txt: string): MusicScore[] {
+  const scoreReg: RegExp = new RegExp(
+    "([a-zA-Z]:.*\\s)*[a-gA-G|^_=',/(2|3|4|6|8|16|32)\\s]*",
+    'gm'
+  );
   const infoReg: RegExp = new RegExp('^[A-Z]:.+', 'gm');
   const musicReg: RegExp = new RegExp(
     "^((?!:)\\s?[_=^]*([A-Za-z]|[|()])[',]*([2-9]|/[2-9])?\\s?(?!:))+",
     'gm'
   );
-  return {
-    musicInfo: parseInfo(txt.match(infoReg).join()),
-    music: parseMusic(txt.match(musicReg).join()),
-  };
+  console.log();
+  const scoreList: MusicScore[] = [];
+  txt.match(scoreReg).forEach((score) => {
+    if (score) {
+      scoreList.push({
+        ...parseInfo(score.match(infoReg).join()),
+        music: parseMusic(score.match(musicReg).join()),
+      });
+    }
+  });
+  return scoreList;
 }
 
 /**
@@ -22,7 +33,7 @@ export function parse(txt: string): MusicScore {
  * @param {string} infoTxt
  * @return {MusicInfo}
  */
-export function parseInfo(infoTxt: string): MusicInfo {
+export function parseInfo(infoTxt: string): MusicScore {
   const info = {
     name: /(?<=T\:)[^\,]*/.exec(infoTxt)[0],
     beat: /(?<=M\:)[^\,]*/.exec(infoTxt)[0],
@@ -40,38 +51,35 @@ export function parseInfo(infoTxt: string): MusicInfo {
  */
 export function parseMusic(musicTxt: string): Note[] {
   const musicScoreArr = musicTxt.split(/\s/);
-  const noteList : Note[] = [];
+  const noteList: Note[] = [];
   for (let index = 0; index < musicScoreArr.length; index++) {
     const note = musicScoreArr[index];
-    console.log(note)
+    if (!note) continue;
+    console.log(note);
     switch (note) {
-      case '(':break;
-      case ')':break;
-      case '|':break;
-      case '||':break;
-      default:
-        const pitch = /[A-Za-z]/.exec(note)[0];// 音调
-        const operation = /[/]/.test(note);// 是否有/号
-        const value = Number(/[2-9]/.exec(note)?.[0]||1);// 与目前时长相差倍数
-        const tone = note.match(/[',]/g)||[];// 是否有升降八度
-        const toneCount = tone.length;// 升降八度数量
-        const modifiedTone = note.match(/[_^]/g)||[];// 是否有升降调
-        const modifiedToneCount = modifiedTone.length;// 升降调数量
-        let fullPitch = pitch;
-        if(modifiedToneCount){
-          const isToneDown = modifiedTone[0]==="_";
-          fullPitch+=`${isToneDown?'b':'s'}`
-        }
-        if(toneCount){
-          const isToneAdd = tone[0]==="'";
-          fullPitch+=`${isToneAdd?3+toneCount:3-toneCount}`
-        }else{
-          fullPitch+=`3`
-        }
+      case '(':
+        break;
+      case ')':
+        break;
+      case '|':
         noteList.push({
-          pitch:fullPitch,
-          playTime:operation?0.25/value:0.25*value
-        })
+          type: 'bar-line',
+        });
+        break;
+      case '||':
+        noteList.push({
+          type: 'bold-double-bar-line',
+        });
+        break;
+      default:
+        const pitch = /[A-Za-z]/.exec(note)[0];
+        const operation = /[/]/.test(note);
+        const value = Number(/[2-9]/.exec(note)?.[0] || 1);
+        noteList.push({
+          type: 'note',
+          pitch: pitch,
+          duration: `${operation ? '1/' : ''}${value}`,
+        });
         break;
     }
   }
--- src/type.ts
+++ src/type.ts
@@ -1,17 +1,20 @@
 export interface MusicScore {
-  musicInfo:MusicInfo, // 乐谱信息
-  music:Note[] // 乐谱
-}
-
-export interface MusicInfo {
-  name:string, // 名称
-  beat:string, // 节拍
-  spend:string, // 速度
-  duration:string, // 默认音符时值
-  tone:string // 调号
+  name?: string; // 名称
+  beat?: string; // 节拍
+  spend?: string; // 速度
+  duration?: string; // 默认音符时值
+  tone?: string; // 调号
+  music?: Note[]; // 乐谱
 }
 
 export interface Note {
-  pitch:string, // 音高
-  playTime:number, // 时值
+  type?: 'note' | 'bar-line' | 'double-bar-line' | 'bold-double-bar-line'; // 类型
+  pitch?: string; // 音高
+  duration?: string; // 时值
+  sharp?: boolean; // 升号
+  flat?: boolean; // 降号
+  'double-sharp'?: boolean; // 重升号
+  'double-flat'?: boolean; // 重降号
+  nature?: boolean; // 还原号
+  register?: number; // 音区
 }
