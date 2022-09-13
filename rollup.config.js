24310bc10d067ceedacdf963a758c5cbc738dd80
shatong <843391622@qq.com>
2022-09-13 15:00:11 +0800

    feat：添加代码打包 banner

--- rollup.config.js
+++ rollup.config.js
@@ -3,6 +3,16 @@ import commonjs from 'rollup-plugin-commonjs';
 import typescript from 'rollup-plugin-typescript';
 import dts from 'rollup-plugin-dts';
 import { defineConfig } from 'rollup';
+import pkg from './package.json';
+
+// 代码头
+const banner = `/*!
+* @encaik/abc v${pkg.version}
+* (c) 2022-${new Date().getFullYear()} Encaik
+* https://github.com/Encaik/abc
+* Released under the MIT License.
+*/
+`;
 
 export default defineConfig([
   {
@@ -11,18 +21,22 @@ export default defineConfig([
       {
         name: 'bundle.esm',
         file: 'dist/bundle.esm.js',
+        banner,
         format: 'module',
       },
       {
         name: 'bundle.umd',
         file: 'dist/bundle.umd.js',
+        banner,
         format: 'umd',
       },
     ],
     plugins: [
       resolve(), // 查找和打包node_modules中的第三方模块
       commonjs(), // 将 CommonJS 转换成 ES2015 模块供 Rollup 处理
-      typescript(), // 解析TypeScript
+      typescript({
+        tsconfig: './tsconfig.json',
+      }), // 解析TypeScript
     ],
   },
   {
